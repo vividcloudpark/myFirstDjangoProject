0ea4863bdc040b21de7e311dfb1f93e7dd8cd6a9
cloud.park <vividxenon@gmail.com>
2022-04-23 12:40:21 +0900

    CBV로 페이지 만들기 - 1) URls에 (class.as_view())로 불러오기   2) views에 class 생성.   3) class에 맞는 Template를 자동으로 찾으려고한다.

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+class PostList(ListView):
+    model = Post
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
+    # template name을 생략하면 post_list.html을 찾는다.
+    # template_name = 'blog/index.html'
+
+    ordering = '-pk'
+#
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
+
+class PostDetail(DetailView):
+    model = Post
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
