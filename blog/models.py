03723266763d11573aece56135166a64fe0da9d1
Cloud Park <vividxenon@gmail.com>
2022-04-27 15:59:15 +0900

    미리보기와 요약문 추가

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
     created_at = models.DateTimeField(auto_now_add=True)
